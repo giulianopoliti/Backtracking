fa329fa50f1544d819c145ae1ed3795019fc2fda
Matias Barros <matiasabarros94@gmail.com>
2024-10-29 13:01:25 -0300

    Initial Commit

---
+++ src/Project/PlanificarCultivosImplementacion.java
@@ -0,0 +1,16 @@
+package Project;
+
+import Lib.Cultivo;
+import Lib.CultivoSeleccionado;
+import Lib.PlanificarCultivos;
+
+import java.util.List;
+
+public class PlanificarCultivosImplementacion implements PlanificarCultivos {
+
+    @Override
+    public List<CultivoSeleccionado> obtenerPlanificacion(List<Cultivo> cultivosDisponibles, double[][] riesgos, String temporada) {
+        // TODO
+        return null;
+    }
+}
