0e345f95fcc472d2850191bb2d390260b12c6c15
Simon Forteza <fortezasimon@gmail.com>
2024-11-26 10:23:57 -0300

    cambio estetico

--- src/Project/PlanificarCultivosImplementacion.java
+++ src/Project/PlanificarCultivosImplementacion.java
@@ -34,88 +34,6 @@ public class PlanificarCultivosImplementacion implements PlanificarCultivos {
         }
         return cultivoSeleccionados;
     }
-    private List<CultivoSeleccionado> backtrackingRepetidos(
-            List<Cultivo> cultivosDisponibles,
-            List<CultivoSeleccionado> cultivoSeleccionados,
-            double[][] riesgos,
-            double gananciaParcial,
-            double mejorGananciaAnterior,
-            int indiceCultivo, //nivel
-            String[][] matrizCultivos) {
-
-        // Si hemos explorado todos los cultivos
-        if (indiceCultivo >= cultivosDisponibles.size()) {
-            return new ArrayList<>(cultivoSeleccionados);
-        }
-
-        Cultivo cultivoActual = cultivosDisponibles.get(indiceCultivo);
-        System.out.println("\nExplorando repetición de cultivo: " + cultivoActual.getNombre());
-
-        List<CultivoSeleccionado> mejorSeleccion = new ArrayList<>(cultivoSeleccionados);
-
-        // Evaluar todos los posibles rectángulos en la matriz para repetir el cultivo
-        for (int i = 0; i < riesgos.length; i++) {
-            for (int j = 0; j < riesgos[i].length; j++) {
-                if (matrizCultivos[i][j] != null) continue; // Saltar si el espacio ya está ocupado
-
-                for (int alto = 1; alto <= Math.min(11, riesgos.length - i); alto++) {
-                    for (int ancho = 1; ancho + alto <= Math.min(11, riesgos[0].length - j); ancho++) {
-
-                        Coordenada arribaIzq = new Coordenada(i, j);
-                        Coordenada abajoDerecha = new Coordenada(i + alto - 1, j + ancho - 1);
-
-                        // Verificar si el área es válida para plantar el cultivo actual
-                        if (abajoDerecha.getX() < riesgos.length && abajoDerecha.getY() < riesgos[0].length) {
-                            if (Utils.sePuedePlantar(arribaIzq, abajoDerecha, matrizCultivos, cultivoActual.getNombre())) {
-
-                                // Calcular ganancia, monto invertido y riesgo asociado para el área
-                                double gananciaArea = calcularGananciaArea(cultivoActual, arribaIzq, abajoDerecha, riesgos);
-                                double montoInvertido = calcularMontoInvertido(cultivoActual, arribaIzq, abajoDerecha);
-                                double riesgoAsociado = RiesgoAsociado(arribaIzq, abajoDerecha, riesgos);
-
-                                // Si la ganancia de esta área es mejor, actualizamos la selección
-                                if (gananciaArea > mejorGananciaAnterior) {
-                                    mejorGananciaAnterior = gananciaArea;
-
-                                    CultivoSeleccionado cultivoSeleccionado = new CultivoSeleccionado(
-                                            cultivoActual.getNombre(), arribaIzq, abajoDerecha,
-                                            montoInvertido, (int) riesgoAsociado, mejorGananciaAnterior
-                                    );
-
-                                    // Marcar el área en la matriz y agregar el cultivo a la selección
-                                    Utils.marcarMatrizCultivos(cultivoActual, arribaIzq, abajoDerecha, matrizCultivos);
-                                    cultivoSeleccionados.add(cultivoSeleccionado);
-
-                                    System.out.println("Cultivo repetido en el área: " + arribaIzq + " a " + abajoDerecha);
-                                    System.out.println("Ganancia actualizada: " + mejorGananciaAnterior);
-
-                                    // Llamada recursiva
-                                    List<CultivoSeleccionado> resultadoRecursivo = backtrackingRepetidos(
-                                            cultivosDisponibles, cultivoSeleccionados, riesgos,
-                                            gananciaParcial + gananciaArea, mejorGananciaAnterior,
-                                            indiceCultivo + 1, matrizCultivos
-                                    );
-
-                                    // Actualizar la mejor selección si corresponde
-                                    if (resultadoRecursivo.size() > mejorSeleccion.size()) {
-                                        mejorSeleccion = resultadoRecursivo;
-                                    }
-
-                                    // Desmarcar la matriz y remover el cultivo para backtracking
-                                    Utils.desmarcarMatrizCultivos(arribaIzq, abajoDerecha, matrizCultivos);
-                                    cultivoSeleccionados.remove(cultivoSeleccionado);
-                                    System.out.println("Retrocediendo y desmarcando el área: " + arribaIzq + " a " + abajoDerecha);
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-        // Retornar la mejor selección después de explorar todas las opciones
-        return mejorSeleccion;
-    }
 
     private List<CultivoSeleccionado> backtracking(
             List<Cultivo> cultivosDisponibles,
@@ -225,6 +143,90 @@ public class PlanificarCultivosImplementacion implements PlanificarCultivos {
         // Paso 8: Retornamos el mejor resultado encontrado entre colocar o no colocar el cultivo
         System.out.println("Retornando el mejor resultado entre colocar y no colocar el cultivo " + cultivoActual.getNombre());
         return resultadoSinColocar.size() > mejorSeleccion.size() ? resultadoSinColocar : mejorSeleccion;
+    }
+
+
+    private List<CultivoSeleccionado> backtrackingRepetidos(
+            List<Cultivo> cultivosDisponibles,
+            List<CultivoSeleccionado> cultivoSeleccionados,
+            double[][] riesgos,
+            double gananciaParcial,
+            double mejorGananciaAnterior,
+            int indiceCultivo, //nivel
+            String[][] matrizCultivos) {
+
+        // Si hemos explorado todos los cultivos
+        if (indiceCultivo >= cultivosDisponibles.size()) {
+            return new ArrayList<>(cultivoSeleccionados);
+        }
+
+        Cultivo cultivoActual = cultivosDisponibles.get(indiceCultivo);
+        System.out.println("\nExplorando repetición de cultivo: " + cultivoActual.getNombre());
+
+        List<CultivoSeleccionado> mejorSeleccion = new ArrayList<>(cultivoSeleccionados);
+
+        // Evaluar todos los posibles rectángulos en la matriz para repetir el cultivo
+        for (int i = 0; i < riesgos.length; i++) {
+            for (int j = 0; j < riesgos[i].length; j++) {
+                if (matrizCultivos[i][j] != null) continue; // Saltar si el espacio ya está ocupado
+
+                for (int alto = 1; alto <= Math.min(11, riesgos.length - i); alto++) {
+                    for (int ancho = 1; ancho + alto <= Math.min(11, riesgos[0].length - j); ancho++) {
+
+                        Coordenada arribaIzq = new Coordenada(i, j);
+                        Coordenada abajoDerecha = new Coordenada(i + alto - 1, j + ancho - 1);
+
+                        // Verificar si el área es válida para plantar el cultivo actual
+                        if (abajoDerecha.getX() < riesgos.length && abajoDerecha.getY() < riesgos[0].length) {
+                            if (Utils.sePuedePlantar(arribaIzq, abajoDerecha, matrizCultivos, cultivoActual.getNombre())) {
+
+                                // Calcular ganancia, monto invertido y riesgo asociado para el área
+                                double gananciaArea = calcularGananciaArea(cultivoActual, arribaIzq, abajoDerecha, riesgos);
+                                double montoInvertido = calcularMontoInvertido(cultivoActual, arribaIzq, abajoDerecha);
+                                double riesgoAsociado = RiesgoAsociado(arribaIzq, abajoDerecha, riesgos);
+
+                                // Si la ganancia de esta área es mejor, actualizamos la selección
+                                if (gananciaArea > mejorGananciaAnterior) {
+                                    mejorGananciaAnterior = gananciaArea;
+
+                                    CultivoSeleccionado cultivoSeleccionado = new CultivoSeleccionado(
+                                            cultivoActual.getNombre(), arribaIzq, abajoDerecha,
+                                            montoInvertido, (int) riesgoAsociado, mejorGananciaAnterior
+                                    );
+
+                                    // Marcar el área en la matriz y agregar el cultivo a la selección
+                                    Utils.marcarMatrizCultivos(cultivoActual, arribaIzq, abajoDerecha, matrizCultivos);
+                                    cultivoSeleccionados.add(cultivoSeleccionado);
+
+                                    System.out.println("Cultivo repetido en el área: " + arribaIzq + " a " + abajoDerecha);
+                                    System.out.println("Ganancia actualizada: " + mejorGananciaAnterior);
+
+                                    // Llamada recursiva
+                                    List<CultivoSeleccionado> resultadoRecursivo = backtrackingRepetidos(
+                                            cultivosDisponibles, cultivoSeleccionados, riesgos,
+                                            gananciaParcial + gananciaArea, mejorGananciaAnterior,
+                                            indiceCultivo + 1, matrizCultivos
+                                    );
+
+                                    // Actualizar la mejor selección si corresponde
+                                    if (resultadoRecursivo.size() > mejorSeleccion.size()) {
+                                        mejorSeleccion = resultadoRecursivo;
+                                    }
+
+                                    // Desmarcar la matriz y remover el cultivo para backtracking
+                                    Utils.desmarcarMatrizCultivos(arribaIzq, abajoDerecha, matrizCultivos);
+                                    cultivoSeleccionados.remove(cultivoSeleccionado);
+                                    System.out.println("Retrocediendo y desmarcando el área: " + arribaIzq + " a " + abajoDerecha);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        // Retornar la mejor selección después de explorar todas las opciones
+        return mejorSeleccion;
     }
 
 
